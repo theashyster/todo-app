aacf565c1b4f00a92fa0becb0bf1df867aa8dc57
Andris Nolendorfs <andris.nolendorfs@gmail.com>
2025-03-22 17:08:06 +0200

    Fix backend URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const PROD_URL = 'http://3.74.160.42';
+export const PROD_URL = 'https://3tytgtfpx4.execute-api.eu-central-1.amazonaws.com';
 export const BASE_URL = 'http://localhost:3100';
 export const APP_URL = '/todo-app';
 export const API_PATH = `/api/todos`;
